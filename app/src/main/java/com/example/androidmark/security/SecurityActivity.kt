565c842773935291e3f168e9cfb5b324470ebfd0
ngliaxl <ngliaxl@gmail.com>
2020-06-02 14:24:43 +0800

    security

--- app/src/main/java/com/example/androidmark/security/SecurityActivity.kt
+++ app/src/main/java/com/example/androidmark/security/SecurityActivity.kt
@@ -1,13 +1,41 @@
 package com.example.androidmark.security
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.security.crypto.EncryptedSharedPreferences
 import com.example.androidmark.BaseActivity
 import com.example.androidmark.R
 
 class SecurityActivity : BaseActivity() {
 
+    private val fileName: String = "file-security"
+    private val masterKeyAlias: String = "master-key-alias"
+    private lateinit var sharedPrefsEditor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_security)
+
+        initPreferences()
+
+
+        sharedPrefsEditor.putBoolean("one", true);
+        sharedPrefsEditor.putString("two", "test")
+
+    }
+
+
+    private fun initPreferences() {
+        val sharedPreferences = EncryptedSharedPreferences
+            .create(
+                fileName,
+                masterKeyAlias,
+                this,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+
+        sharedPrefsEditor = sharedPreferences.edit()
+
     }
 }
