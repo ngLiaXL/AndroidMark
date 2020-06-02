db391b866c0d78bd6a2f62ec544a48dd822741f5
Liang <xianglong.liang@accenture.com>
2020-06-02 14:12:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidmark/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.androidmark
+
+import android.content.Intent
+import android.os.Bundle
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.example.androidmark.security.SecurityActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = Adapter()
+        mainRecyclerView.adapter = adapter
+
+        adapter.onItemChildClickListener =
+            BaseQuickAdapter.OnItemChildClickListener() { _, view, position ->
+                val action = adapter.getItem(position)?.cls
+                startActivity(Intent(this@MainActivity, action))
+
+            }
+        addFunction(adapter)
+    }
+
+    private fun addFunction(adapter: Adapter) {
+        adapter.addData(Item("Security", SecurityActivity::class.java))
+    }
+
+
+    class Adapter : BaseQuickAdapter<Item, BaseViewHolder>(R.layout.layout_fun_item) {
+
+        override fun convert(helper: BaseViewHolder, item: Item) {
+            helper.setText(R.id.tv_fun_name, item.name)
+            helper.addOnClickListener(R.id.tv_fun_name)
+        }
+
+    }
+
+    data class Item(val name: String, val cls: Class<out BaseActivity>)
+}
---
+++ app/src/main/java/com/example/androidmark/security/SecurityActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidmark.security
+
+import android.os.Bundle
+import com.example.androidmark.BaseActivity
+import com.example.androidmark.R
+
+class SecurityActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_security)
+    }
+}
